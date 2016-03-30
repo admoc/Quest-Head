0f163eaedb0667f63261f7da0389844d3ae181f5
tomaszWoloszyk <towoloszyk@wi.zut.edu.pl>
2016-03-31 00:19:14 +0200

    add item class to model

---
+++ QuestHeadProject/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace QuestHeadProject.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+    }
+
+    public class ItemDBCtxt : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+    }
+}
