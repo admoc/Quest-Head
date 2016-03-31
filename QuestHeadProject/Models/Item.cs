59f60dbd4e8bbcb41e56b5fee6d92f00ae57675c
admoc <adam.rf15@gmail.com>
2016-03-31 10:32:27 +0200

    adding database

--- QuestHeadProject/Models/Item.cs
+++ QuestHeadProject/Models/Item.cs
@@ -9,6 +9,7 @@ namespace QuestHeadProject.Models
     public class Item
     {
         public int Id { get; set; }
+        public string Name { get; set; }
     }
 
     public class ItemDBCtxt : DbContext
